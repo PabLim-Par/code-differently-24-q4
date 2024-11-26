e4f8be5469ca766ed7d15236cb44a70eb5fc8fab
PabLim-Par <plimon244@gmail.com>
2024-11-26 14:28:07 +0000

    Feat: adds getting specific id in items function

--- lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
+++ lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
@@ -1,15 +1,19 @@
 package com.codedifferently.lesson26.web;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.codedifferently.lesson26.library.Librarian;
 import com.codedifferently.lesson26.library.Library;
 import com.codedifferently.lesson26.library.MediaItem;
 import com.codedifferently.lesson26.library.search.SearchCriteria;
-import java.io.IOException;
-import java.util.List;
-import java.util.Set;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
@@ -29,4 +33,16 @@ public class MediaItemsController {
     var response = GetMediaItemsResponse.builder().items(responseItems).build();
     return response;
   }
+  @GetMapping("/items/{id}")
+  public GetMediaItemsResponse getSpecificItems(@PathVariable UUID id){
+    Set<MediaItem> items = library.search(SearchCriteria.builder().id(id.toString()).build());
+    List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
+    var response = GetMediaItemsResponse.builder().items(responseItems).build();
+    return response;
+  }
+  
+
+//   @PostMapping("./items")
+
+//   @DeleteMapping("./items/:id")
 }
